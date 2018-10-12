6dbff4ee77ce646fc7fb1dd41ba3d1e07be4aaa8
chanukaDissanayaka <chanuka04heshan@gmail.com>
2018-10-12 11:14:48 +0530

    virtual joystick

---
+++ Morphia virtual joystick/Assets/sphere.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sphere : MonoBehaviour {
+
+    protected Joystick joystick;
+    protected Joybutton joybutton;
+    protected bool jump;
+    // Use this for initialization
+    void Start () {
+        joystick = FindObjectOfType<Joystick>();
+        joybutton = FindObjectOfType<Joybutton>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        var rigidbody = GetComponent<Rigidbody>();
+        rigidbody.velocity = new Vector3( joystick.Horizontal * 10f /*+ Input.GetAxis("Horizontal")*10f*/,
+                                          rigidbody.velocity.y,
+                                          joystick.Vertical * 10f);
+        if(!jump && joybutton.pressed )
+        {
+            jump = true;
+            rigidbody.velocity += Vector3.up * 10f;
+        }
+        if (jump && !joybutton.pressed)
+        {
+            jump = false;
+        }
+	}
+}
